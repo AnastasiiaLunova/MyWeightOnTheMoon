ef500e34e489589568cfd076ad70e8b017fdb41a
Anastasiia <Anastasiia@DESKTOP-9TQJ1OA>
2018-05-23 20:06:08 +0300

    Gravity on the Moon 17%

---
+++ WeightOnTheMoon/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		MoonWeight objWeight = new MoonWeight();
+		double result = objWeight.getWeight(75);
+		System.out.println(result);
+	}
+
+}
---
+++ WeightOnTheMoon/src/MoonWeight.java
@@ -0,0 +1,10 @@
+
+public class MoonWeight {
+	double getWeight(int i) {
+		double Moon = 17;
+		return i * Moon / 100;
+		
+	}
+	
+
+}
